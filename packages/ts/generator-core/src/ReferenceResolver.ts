6bf09850585eb1d5da610c39d4892692653c804c
Vlad Rindevich <vladrin@vaadin.com>
2025-01-22 13:36:31 +0200

    feat(generator-plugin-transfertypes): add transfer type plugin for MultipartFile

--- packages/ts/generator-core/src/ReferenceResolver.ts
+++ packages/ts/generator-core/src/ReferenceResolver.ts
@@ -1,6 +1,5 @@
 import type SwaggerParser from '@apidevtools/swagger-parser';
 import type { OpenAPIV3 } from 'openapi-types';
-import type { ReadonlyDeep } from 'type-fest';
 
 export default class ReferenceResolver {
   readonly #parser: SwaggerParser;
@@ -10,7 +9,7 @@ export default class ReferenceResolver {
   }
 
   // eslint-disable-next-line @typescript-eslint/ban-types
-  resolve<T extends ReadonlyDeep<object>>(obj: ReadonlyDeep<OpenAPIV3.ReferenceObject> | T): T {
+  resolve<T extends object>(obj: OpenAPIV3.ReferenceObject | T): T {
     return '$ref' in obj ? this.#parser.$refs.get(obj.$ref) : obj;
   }
 }
